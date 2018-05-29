4404f22a6aeadadc0d3042912ca89145af5bf14d
sakshi <sakshikansal08@gmail.com>
2018-05-29 19:15:37 +1000

    commit for mysql

--- src/main/java/com/GKquiz/rootConfig.java
+++ src/main/java/com/GKquiz/rootConfig.java
@@ -4,7 +4,9 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,7 +17,6 @@ import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -27,96 +28,98 @@ import com.data.resultmailservice;
 import com.data.userRoles;
 import com.data.userresult;
 
-
-@Configuration
 @EnableWebMvc
+@Configuration
 @EnableTransactionManagement
 @EnableConfigurationProperties(mailproperties.class)
-@ComponentScan(basePackages={"com.data","com.web"},excludeFilters={@Filter(type=FilterType.ANNOTATION, value=EnableWebMvc.class)})
+@ComponentScan(basePackages = { "com.data", "com.web" }, excludeFilters = {
+		@Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class) })
 public class rootConfig extends WebMvcConfigurerAdapter {
 
 	@Bean
-	public User getUser()
-	{
+	public User getUser() {
 		return new User();
 	}
-	
+
 	@Bean
-	public Questions getQuestionAnswerbean()
-	{
+	public Questions getQuestionAnswerbean() {
 		return new Questions();
 	}
-	
+
 	@Bean
-	public userresult getuserresultbean()
-	{
+	public userresult getuserresultbean() {
 		return new userresult();
 	}
-	
+
 	@Bean
-	public userRoles getUserRoles()
-	{
+	public userRoles getUserRoles() {
 		return new userRoles();
 	}
-	
+
 	@Bean
-	public DataSource getdatasource()
-	{
-		DriverManagerDataSource datasource=new DriverManagerDataSource();
+	public DataSource getdatasource() {
+		DriverManagerDataSource datasource = new DriverManagerDataSource();
 		datasource.setDriverClassName("org.h2.Driver");
-		
+
 		datasource.setUrl("jdbc:h2:~/test;DB_CLOSE_DELAY=-1");
 		datasource.setUsername("sa");
 		datasource.setPassword("sa");
 		return datasource;
 	}
-/*	
-	@Bean
-	public DataSource dataSource() {
-	return new EmbeddedDatabaseBuilder()
-	.setType(EmbeddedDatabaseType.H2)
-		.build();
+	/*
+	 * @Bean public DataSource dataSource() { return new EmbeddedDatabaseBuilder()
+	 * .setType(EmbeddedDatabaseType.H2) .build();
+	 * 
+	 * }
+	 */
 
-}*/
-	
 	@Bean
-	public LocalSessionFactoryBean getSessionFactory()
-	{LocalSessionFactoryBean sessionfactory=new LocalSessionFactoryBean();
-	sessionfactory.setDataSource(getdatasource());
-	sessionfactory.setAnnotatedClasses(User.class);
-	sessionfactory.setAnnotatedClasses(userresult.class);
-	sessionfactory.setAnnotatedClasses(Questions.class);
-	sessionfactory.setAnnotatedClasses(userRoles.class);
-	sessionfactory.setPackagesToScan("com.data");
-	Properties props = new Properties();
-	props.setProperty("dialect", "org.hibernate.dialect.H2Dialect"); 
-	sessionfactory.setHibernateProperties(props);
-		return sessionfactory;
-		
+	public SessionFactory getSessionFactory() {
+
+		org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
+		configuration.configure("hibernate.cfg.xml");
+
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+				.applySettings(configuration.getProperties()).build();
+		SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+		/*
+		 * LocalSessionFactoryBean sessionfactory = new LocalSessionFactoryBean();
+		 * sessionfactory.setDataSource(getdatasource());
+		 * sessionfactory.setAnnotatedClasses(User.class);
+		 * sessionfactory.setAnnotatedClasses(userresult.class);
+		 * sessionfactory.setAnnotatedClasses(Questions.class);
+		 * sessionfactory.setAnnotatedClasses(userRoles.class);
+		 * sessionfactory.setPackagesToScan("com.data"); Properties props = new
+		 * Properties(); props.setProperty("dialect",
+		 * "org.hibernate.dialect.H2Dialect");
+		 * sessionfactory.setHibernateProperties(props);
+		 */
+		return sessionFactory;
+
 	}
-	
-	//@Bean
-	
+
+	// @Bean
+
 	public MailSender mailSender(Environment env) {
-	JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-	mailSender.setHost("smtp.gmail.com");
-    mailSender.setPort(587);
-    mailSender.setUsername("sakshikansal08@gmail.com");
-    mailSender.setPassword("motu0107");
-     
-    Properties props = mailSender.getJavaMailProperties();
-    props.put("mail.transport.protocol", "smtp");
-    props.put("mail.smtp.auth", "true");
-    props.put("mail.smtp.starttls.enable", "true");
-    props.put("mail.debug", "true");
-     
-	return mailSender;
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(587);
+		mailSender.setUsername("sakshikansal08@gmail.com");
+		mailSender.setPassword("motu0107");
+
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "true");
+
+		return mailSender;
 	}
-	
-//	@Bean
-	public resultmailservice getmailservice()
-	{
+
+	// @Bean
+	public resultmailservice getmailservice() {
 		return new resultmailservice();
 	}
-	
+
 }
--- src/test/java/com/examplezone/webexamples/GkQuizApplicationTests.java
+++ src/test/java/com/examplezone/webexamples/GkQuizApplicationTests.java
@@ -2,11 +2,13 @@ package com.examplezone.webexamples;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@SpringBootConfiguration
 public class GkQuizApplicationTests {
 
 	@Test
